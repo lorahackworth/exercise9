19244e86e48d64cd52a81fc5f60c9eae16ab76f7
lorahackworth <lora.g.hackworth@gmail.com>
2024-03-12 13:23:36 -0400

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Phone num1 = new Phone("614222222");
+        Email em1 = new Email("John@john.com");
+
+        num1.contact();
+        em1.contact();
+
+    }
+}
+
+abstract class Contacts{
+    abstract public void contact();
+
+
+}
+
+class Email extends Contacts{
+    private String email;
+
+    public Email(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void contact() {
+        System.out.println("Emailing " + email);
+    }
+}
+
+class Phone extends Contacts{
+    private String phone;
+
+    public Phone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public void contact() {
+        System.out.println("Calling " + phone);
     }
 }
